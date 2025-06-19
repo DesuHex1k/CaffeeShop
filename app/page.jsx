191e64335095b5f52d4c38671794464164ef70af
Vt231Sanya <vt231_noo@student.ztu.edu.ua>
2025-06-20 00:45:49 +0300

    Продукти + бронювання

--- app/page.jsx
+++ app/page.jsx
@@ -7,52 +7,75 @@ const Page = () => {
         {
             name: 'Caffe Macchiato',
             description: 'Shot of espresso extracted the Italian way.',
-            price: 15,
+            price: 65,
             image: 'https://static.vecteezy.com/system/resources/previews/029/345/013/original/macchiato-coffee-isolated-on-transparent-background-ai-generative-free-png.png',
         },
         {
             name: 'Caffe Latte',
             description: 'Espresso and steamed milk with foamed.',
-            price: 20,
+            price: 70,
             image: 'https://www.pngarts.com/files/1/Cafe-Latte-PNG-Image.png',
         },
         {
             name: 'Espresso Macchiato',
             description: 'Espresso marked with foamed milk.',
-            price: 25,
+            price: 75,
             image: 'https://png.pngtree.com/png-vector/20240131/ourmid/pngtree-espresso-macchiato-coffee-drink-with-foamed-milk-png-image_11520214.png',
         },
         {
             name: 'Caffe Americano',
             description: 'Espresso topped with hot water.',
-            price: 18,
+            price: 68,
             image: 'https://static.vecteezy.com/system/resources/previews/023/438/445/non_2x/espresso-coffee-cutout-free-png.png',
         },
         {
             name: 'Hot Chocolate',
             description: 'Real chocolate blended with steamed milk.',
-            price: 25,
+            price: 75,
             image: 'https://static.vecteezy.com/system/resources/previews/028/174/738/original/cup-of-hot-chocolate-with-cream-isolated-on-transparent-background-generative-ai-png.png',
         },
         {
             name: 'Strawberry Milk',
             description: 'Real Strawberry blended with steamed milk.',
-            price: 15,
+            price: 65,
             image: 'https://th.bing.com/th/id/R.50b8c69584ce0c55986f91b565dfe78e?rik=xtp54tNzSPHEZQ&pid=ImgRaw&r=0',
         },
         {
             name: 'Asian Lemon Tea',
             description: 'Ask your Barista about our selection of teas.',
-            price: 10,
+            price: 60,
             image: 'https://static.vecteezy.com/system/resources/previews/038/370/243/non_2x/ai-generated-iced-lemon-tea-isolated-on-transparent-background-png.png',
         },
         {
             name: 'Black Asian Tea',
             description: 'A blend of black tea, spices, honey and milk.',
-            price: 25,
+            price: 75,
             image: 'https://static.vecteezy.com/system/resources/previews/016/349/669/non_2x/cup-of-tea-with-fresh-tea-leaves-on-saucer-transparant-background-png.png',
         },
     ];
+
+    const products = [
+        {
+            name: 'American coffee',
+            price: 75,
+            image: 'https://unblast.com/wp-content/uploads/2018/08/Coffee-Package-Mockup-PSD-aa.jpg',
+        },
+        {
+            name: 'Cost Rica Light',
+            price: 85,
+            image: 'https://th.bing.com/th/id/R.95c8bf6dd5c1c0fd8deda62b67fda384?rik=sQaCZ3fXDNjLgA&riu=http%3a%2f%2funblast.com%2fwp-content%2fuploads%2f2018%2f08%2fCoffee-Package-Mockup.jpg&ehk=SVeoZH0KWTzZo4wamMNyd3vlame2QEwrM646trzNZxw%3d&risl=&pid=ImgRaw&r=0',
+        },
+        {
+            name: 'Jamaican Mountain',
+            price: 65,
+            image: 'https://unblast.com/wp-content/uploads/2018/08/Coffee-Package-Mockup-PSD-aa.jpg',
+        },
+        {
+            name: 'Ethopian Fancy',
+            price: 75,
+            image: 'https://th.bing.com/th/id/R.95c8bf6dd5c1c0fd8deda62b67fda384?rik=sQaCZ3fXDNjLgA&riu=http%3a%2f%2funblast.com%2fwp-content%2fuploads%2f2018%2f08%2fCoffee-Package-Mockup.jpg&ehk=SVeoZH0KWTzZo4wamMNyd3vlame2QEwrM646trzNZxw%3d&risl=&pid=ImgRaw&r=0',
+        }
+    ]
     return (
         <div className="page">
             <div className="about content">
@@ -98,21 +121,77 @@ const Page = () => {
             <div className="menu_page">
                 <h1>Наше меню</h1>
                 <div className="menu content">
-
-
                     {menuItems.map((item, index) => (
                         <div className="item" key={index}>
                             <img src={item.image} alt={item.name} className="image" />
                             <div className="text">
                                 <div className="header">
                                     <h3>{item.name}</h3>
-                                    <span className="price">${item.price}</span>
+                                    <span className="price">{item.price} грн</span>
                                 </div>
                                 <p>{item.description}</p>
                             </div>
                         </div>
                     ))}
                 </div>
+            </div>
+
+            <div className="shop content">
+                <h1>Наші продукти</h1>
+                <div className="shopItems">
+                    {products.map((item, index) => (
+                        <div className="shopItem" key={index}>
+                            <img src={item.image} alt={item.name} className="shopImage" />
+                            <div className="text">
+                                <div className="header">
+                                    <h3>{item.name}</h3>
+                                    <span className="price">{item.price} грн</span>
+                                </div>
+                            </div>
+                            <button className='cartButton'>До кошику</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="book_page">
+                <div className="book content">
+                    <h1>Заброннюй стіл</h1>
+                    <form action="" className="form">
+                        <div className="grid">
+                            <input type="text" placeholder="Your Name" />
+                            <input type="email" placeholder="Email Address" />
+                            <input type="tel" placeholder="Phone Number" />
+                            <input type="date" placeholder="MM/DD/YYYY" />
+                            <select>
+                                <option>7:00</option>
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                                <option>18:00</option>
+                                <option>19:00</option>
+                            </select>
+                            <select>
+                                <option>01 Людина</option>
+                                <option>02 Людини</option>
+                                <option>03 Людини</option>
+                                <option>04 Людини</option>
+                            </select>
+                        </div>
+                    </form>
+
+                    <textarea placeholder="Your message..." rows="4" />
+
+                    <button type="submit" className='cartButton'>Book Now</button>
+                </div>
+
             </div>
 
         </div>
