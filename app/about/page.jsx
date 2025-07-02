174c5210ed8cd19b8bc049766321fe0d49b82d7b
Sanya <snagy.sasha@gmail.com>
2025-07-02 23:10:14 +0300

    sec page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './page.css'
+import Story from "../../components/Story";
+import Testimonials from "../../components/Testimonials";
 
 const Page = () => {
     return (
         <div className="page">
-            asd
+            <Story/>
+
+            <div className="quote">
+                <h1>
+                "Good ideas start with brainstorming great ideas start with coffee"
+                </h1>
+            </div>
+
+            <div className="reviews">
+                <h1>Відгуки</h1>
+                <Testimonials/>
+            </div>
+
         </div>
     );
 };
--- components/Header.jsx
+++ components/Header.jsx
@@ -7,22 +7,28 @@ import {useState} from "react";
 
 export default function Header() {
     const pathname = usePathname();
-    const [menuOpen, setMenuOpen] = useState(true);
+    // const [menuOpen, setMenuOpen] = useState(true);
 
-    const navLinks = [
+    const navLinksLeft = [
         { href: '/', label: 'Головна' },
         { href: '/about', label: 'Про нас' },
         { href: '/menu', label: 'Меню' },
         { href: '/booking', label: 'Бронювання' },
     ];
+    const navLinksRight = [
+        { href: '/reviews', label: 'Відгуки' },
+        { href: '/shop', label: 'Магазин' },
+        { href: '/contact', label: 'Контакти' },
+        {href: '/cart', label: 'Кошик'},
+    ];
     return (
         <header className={styles.mainHeader}>
-            <div className={styles.logoOnly} onClick={() => setMenuOpen(prev => !prev)}>
+            <div className={styles.logoOnly}>
                 <Image  src="/logo.png" alt="Logo" width={80} height={80} />
             </div>
-                <nav className={`${styles.navBar} ${menuOpen ? styles.open : styles.closed}`}>
+                <nav className={`${styles.navBar}`}>
                 <ul className={styles.navLeft}>
-                    {navLinks.map(link => (
+                    {navLinksLeft.map(link => (
                         <li key={link.href}>
                             <Link
                                 href={link.href}
@@ -39,28 +45,16 @@ export default function Header() {
                 </div>
 
                 <ul className={styles.navRight}>
-                    <li><Link href="/reviews">Відгуки</Link></li>
-                    <li><Link href="/shop">Магазин</Link></li>
-                    <li><Link href="/contact">Контакти</Link></li>
-                    <li>
-                        <Link href="/search">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                 className="searchIcon" viewBox="0 0 16 16">
-                                <path
-                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                            </svg>
-                        </Link>
-                    </li>
-                    <li className={styles.cart}>
-                        <Link href="/cart">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                 className={styles.bi} viewBox="0 0 16 16">
-                                <path
-                                    d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                            </svg>
-                            <span className={styles.cartCount}>2</span>
-                        </Link>
-                    </li>
+                    {navLinksRight.map(link => (
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className={pathname === link.href ? styles.active : ''}
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
